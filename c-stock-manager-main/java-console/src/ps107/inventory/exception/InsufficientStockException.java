05d2f8eafd1f2d6804b2291abb33f81df358c204
BoddigaSaikrishna <2300039074@kluniversity.in>
2026-09-19 15:29:43 +0530

    Remove Java subsystem completely and clean up Lovable references

--- c-stock-manager-main/java-console/src/ps107/inventory/exception/InsufficientStockException.java
+++
@@ -1,7 +0,0 @@
-package ps107.inventory.exception;
-
-public final class InsufficientStockException extends Exception {
-    public InsufficientStockException(String sku, int available, int requested) {
-        super("Insufficient stock for SKU " + sku + ": available=" + available + ", requested=" + requested);
-    }
-}
